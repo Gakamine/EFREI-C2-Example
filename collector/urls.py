e08b81308bca167434f54bb2b0b2d3d610ecb8ed
Gakamine <jolyantoine@pm.me>
2023-01-25 05:48:19 -0500

    Update readme + allow base64 in URL

--- collector/urls.py
+++ collector/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('<hostname>/<ip>/<data_type>/<data>/<file_type>', views.index),
-    path('<hostname>/<ip>/<data_type>/<data>', views.index),
+    path('<hostname>/<ip>/<data_type>/<file_type>/<path:data>/', views.index),
+    path('<hostname>/<ip>/<data_type>/<path:data>/', views.index),
     path('<hostname>/<ip>/<data_type>', views.index),
     path('<hostname>/<ip>/', views.index),
     path('<hostname>/', views.index),
